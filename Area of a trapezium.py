997fc7d19ea1d25783170bb8ed14d057c362c098
lemonntec <miraclechinedum1@gmail.com>
2023-10-10 12:03:34 +0100

    Area of a trapezium

---
+++ Area of a trapezium.py
@@ -0,0 +1,8 @@
+#the inputs are already in metres
+a = float(input('input a: '))
+b = float(input('input b: '))
+height = float(input('input height: '))
+
+area = float((a+b)/2 *height)
+
+print('The Area of the trapezium is',area,'metre')
